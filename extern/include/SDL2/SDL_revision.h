ce98f4b333cecdeb3ac13489797e57d3ac02b116
ftcister <felipe.cisternas.alvarez@hotmail.com>
2022-07-05 11:02:42 -0400

    add dependencies

---
+++ extern/include/SDL2/SDL_revision.h
@@ -0,0 +1,6 @@
+#define SDL_REVISION "https://github.com/libsdl-org/SDL@ded6c22efb3e11ee9a6df5778aeec38cd5d8027d"
+#define SDL_REVISION_NUMBER 0
+
+#ifndef SDL_REVISION
+#define SDL_REVISION ""
+#endif
